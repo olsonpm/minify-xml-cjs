dd0906a0716975cc98c6b2378f633471d0795090
Phil Olson <olsonpm@users.noreply.github.com>
2023-11-13 13:11:01 -0600

    account for named exports

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('fix-esm').require('minify-xml')
+const defaultExport = require('fix-esm').require('minify-xml')
+
+const { minify, ...restOfExport } = defaultExport
+
+for (const [k, v] of Object.entries(restOfExport)) {
+  minify[k] = v
+}
+
+module.exports = minify
